0efefcc8648b3a3822b0b97e937276566a83bd2d
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-04-18 19:06:47 -0700

    added just the app.get for fetching the db.json. pausing for now to figure out rest of routes

--- helpers/routes/api.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const notes = require('../../db/db.json')
-const uuid = require('../../helpers/uuid')
-const express = require('express')
-const app = express()
-
-app.get("/notes", (req, res) => {
-    res.json(notesArray);
-})
--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 const express = require('express');
+
 const app = express()
-require('./helpers/routes/api')
-require('./helpers/routes/html')
+
+
 
 
 const PORT = 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static((__dirname, 'public')))
+app.use(express.static(('public')))
+const api = require('./routes/api.js')(app)
+const html = require('./routes/html.js')(app)
 
 
 app.get('/notes', (req,res) => {
     res.sendFile(__dirname + '/public/notes.html')
 })
-
-
-app.get('*', (req, res) => 
-res.console.error(404));
 
 
 app.listen(PORT)
