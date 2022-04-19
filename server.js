77e8b37ad1e45651bfbe3d233629ab3033547965
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-04-19 14:51:30 -0700

    restructuring files.. keep getting errors when i split up my routes

--- server.js
+++ server.js
@@ -3,21 +3,32 @@ const express = require('express');
 const app = express()
 
 
-
-
 const PORT = 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(('public')))
-const api = require('./routes/api.js')(app)
-const html = require('./routes/html.js')(app)
+require('./routes/api.js')(app)
+require('./routes/html.js')(app)
 
 
+//makes the main page button work 
 app.get('/notes', (req,res) => {
     res.sendFile(__dirname + '/public/notes.html')
 })
 
+// returning the database for displaying saved notes. 
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+// wildcard route to redirect to main page in event of mistyped url
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+
+});
+
+//writing a post to save the data. 
 
 app.listen(PORT)
 console.log("now listening on port 3000")
